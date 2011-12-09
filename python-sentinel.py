2ed6c8c0e3c0f665ea0c3b2f5f74791d49698028
Alexander Khryukin <alexander@mezon.ru>
2011-12-09 15:58:26 +0400

    man at work

--- python-sentinel.py
+++ python-sentinel.py
@@ -1,11 +1,50 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
 import paramiko
-ssh=paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect("note2",username="fdkit",password="rosa", port=22)
-stdin, stdout, stderr = ssh.exec_command("cat /var/log/syslog")
-result = stdout.read().splitlines()
-print result
-#ssh.close()
+from threading import Thread
+import subprocess
+from Queue import Queue
+import sys, os, os.path, socket
+
+num_threads = 4
+queue = Queue()
+#ips = ["olololdddd", "google.com", "mail.ru", "yandex.ru"]
+ips = ["mail.ru"]
+def pinger(i, q):
+# ping server
+    while True:
+        ip = q.get()
+        print "Thread %s: Pinging %s" % (i, ip)
+        ret = subprocess.call("ping -c 5 %s" % ip,
+                        shell=True,
+                        stdout=open('/dev/null', 'w'),
+                        stderr=subprocess.STDOUT)
+        if ret == 0:
+            print "%s: working fine." % ip
+	else:
+	    print "%s: не отвечает" % ip
+	    print "How about slap your shiti, %s"
+	    ssh=paramiko.SSHClient()
+	    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	    ssh.connect("note2",username="root",password="rosa", port=22)
+	    stdin, stdout, stderr = ssh.exec_command("cat /var/log/pm-powersave.log")
+	    result = stdout.read().splitlines()
+	    print result
+	    ssh.close()
+	q.task_done()
+
+#Spawn thread pool
+for i in range(num_threads):
+
+	worker = Thread(target=pinger, args=(i, queue))
+	worker.setDaemon(True)
+	worker.start()
+
+#Place work in queue
+for ip in ips:
+	queue.put(ip)
+
+#Wait until worker threads are done to exit    
+queue.join()
